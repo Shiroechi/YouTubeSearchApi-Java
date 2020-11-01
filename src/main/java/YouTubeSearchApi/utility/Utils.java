82d47dfd9a173647c20ec89e9ff663159acf9a58
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-11-01 19:54:00 +0700

    Update Utils.java

--- src/main/java/YouTubeSearchApi/utility/Utils.java
+++ src/main/java/YouTubeSearchApi/utility/Utils.java
@@ -21,9 +21,6 @@ public class Utils {
     public static String httpRequest(String url) throws IOException {
         URLConnection connection = new URL(url).openConnection();
 
-        connection.setRequestProperty("User-Agent",
-                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
-
         connection.connect();
         
         InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream(),
