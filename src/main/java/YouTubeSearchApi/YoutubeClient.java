51d4e3489c73dc1ffda1d50dfa2384586ade3257
Restyled.io <commits@restyled.io>
2020-11-17 08:20:50 +0000

    Restyled by clang-format

--- src/main/java/YouTubeSearchApi/YoutubeClient.java
+++ src/main/java/YouTubeSearchApi/YoutubeClient.java
@@ -8,7 +8,6 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -19,131 +18,149 @@ import java.util.regex.Pattern;
 
 public class YoutubeClient {
 
-    private String YOUTUBE_BASE_URL;
+  private String YOUTUBE_BASE_URL;
 
-    public YoutubeClient() {
-        this.YOUTUBE_BASE_URL = "https://www.youtube.com/";
+  public YoutubeClient() { this.YOUTUBE_BASE_URL = "https://www.youtube.com/"; }
+
+  public List<YoutubeVideo> search(String keywords, int maxResults,
+                                   String language)
+      throws IOException, NoResultFoundException {
+    String startFeature = "ytInitialData";
+    String encodedKeywords =
+        URLEncoder.encode(keywords, StandardCharsets.UTF_8.toString());
+    String searchUrl =
+        this.YOUTUBE_BASE_URL + "results?gl=AU&hl=" + language +
+        "&persist_gl=1&persist_hl=1&search_query=" + encodedKeywords;
+
+    // try get feature 3 times
+    String pageContent = "";
+    boolean foundFeatureFlag = false;
+    for (int i = 0; i < 3; i++) {
+      pageContent = Utils.httpRequest(searchUrl);
+      if (pageContent.contains(startFeature)) {
+        foundFeatureFlag = true;
+        break;
+      }
     }
 
-    public List<YoutubeVideo> search(String keywords, int maxResults, String language) throws IOException, NoResultFoundException {
-        String startFeature = "ytInitialData";
-        String encodedKeywords = URLEncoder.encode(keywords, StandardCharsets.UTF_8.toString());
-        String searchUrl = this.YOUTUBE_BASE_URL + "results?gl=AU&hl=" + language
-                + "&persist_gl=1&persist_hl=1&search_query=" + encodedKeywords;
-
-        // try get feature 3 times
-        String pageContent = "";
-        boolean foundFeatureFlag = false;
-        for (int i = 0; i < 3; i++) {
-            pageContent = Utils.httpRequest(searchUrl);
-            if (pageContent.contains(startFeature)) {
-                foundFeatureFlag = true;
-                break;
-            }
-        }
-
-        if (!foundFeatureFlag) {
-            throw new NoResultFoundException("What you searched was unfortunately not found or doesn't exist. keywords: " + keywords);
-        }
-
-
-        // + startFeature.length() + 3. because we want to get rid of the startFeature and " = ".
-        // And get only the Json data
-        int startIndex = pageContent.indexOf(startFeature) + startFeature.length() + 3;
-
-        final String startIndexToEndIndex = pageContent.substring(startIndex, pageContent.length() - 1);
-
-        int endIndex = startIndexToEndIndex.indexOf("};");
-
-        String jsonString = startIndexToEndIndex.substring(0, endIndex) + "}";
-
-        Gson gson = new Gson();
-        JsonObject jsonObject = gson.fromJson(jsonString, JsonObject.class);
-
-        JsonArray contents = jsonObject.get("contents").getAsJsonObject()
-                                  .get("twoColumnSearchResultsRenderer").getAsJsonObject()
-                                  .get("primaryContents").getAsJsonObject()
-                                  .get("sectionListRenderer").getAsJsonObject()
-                                  .get("contents").getAsJsonArray()
-                                  .get(0).getAsJsonObject()
-                                  .get("itemSectionRenderer").getAsJsonObject()
-                                  .get("contents").getAsJsonArray();
-
-        // Flags
-        int counter = 0;
-        List<YoutubeVideo> videos = new ArrayList<YoutubeVideo>();
-
-        for (JsonElement videoJsonElement : contents) {
-            JsonObject videoJsonObject = videoJsonElement.getAsJsonObject();
-            if (videoJsonObject.has("videoRenderer")) {
-                JsonObject videoData = videoJsonObject.get("videoRenderer").getAsJsonObject();
-
-                // Initiate YoutubeVideo object instance & parse video renderer data
-                YoutubeVideo video = YoutubeVideo.getParsedVideoRenderer(videoData);
-
-                videos.add(video);
-
-                counter += 1;
-                if (counter >= maxResults) {
-                    break;
-                }
-            }
-        }
-        return videos;
+    if (!foundFeatureFlag) {
+      throw new NoResultFoundException(
+          "What you searched was unfortunately not found or doesn't exist. keywords: " +
+          keywords);
     }
 
-    public YoutubeVideo getInfoByVideoId(String id) throws IOException {
-        String videoUrl = this.YOUTUBE_BASE_URL + "watch?v=" + id;
-        String requestUrl = "https://noembed.com/embed?url=" + videoUrl;
+    // + startFeature.length() + 3. because we want to get rid of the
+    // startFeature and " = ". And get only the Json data
+    int startIndex =
+        pageContent.indexOf(startFeature) + startFeature.length() + 3;
 
-        String pageContent = Utils.httpRequest(requestUrl);
+    final String startIndexToEndIndex =
+        pageContent.substring(startIndex, pageContent.length() - 1);
 
-        Gson gson = new Gson();
+    int endIndex = startIndexToEndIndex.indexOf("};");
 
-        JsonObject videoObject = gson.fromJson(pageContent, JsonObject.class);
+    String jsonString = startIndexToEndIndex.substring(0, endIndex) + "}";
 
-        return YoutubeVideo.getParsedOEmbedObject(videoObject);
-    }
+    Gson gson = new Gson();
+    JsonObject jsonObject = gson.fromJson(jsonString, JsonObject.class);
 
-    public YoutubeVideo getInfoByVideoUrl(String videoUrl) throws IOException {
-        String requestUrl = "https://noembed.com/embed?url=" + videoUrl;
+    JsonArray contents = jsonObject.get("contents")
+                             .getAsJsonObject()
+                             .get("twoColumnSearchResultsRenderer")
+                             .getAsJsonObject()
+                             .get("primaryContents")
+                             .getAsJsonObject()
+                             .get("sectionListRenderer")
+                             .getAsJsonObject()
+                             .get("contents")
+                             .getAsJsonArray()
+                             .get(0)
+                             .getAsJsonObject()
+                             .get("itemSectionRenderer")
+                             .getAsJsonObject()
+                             .get("contents")
+                             .getAsJsonArray();
 
-        String pageContent = Utils.httpRequest(requestUrl);
+    // Flags
+    int counter = 0;
+    List<YoutubeVideo> videos = new ArrayList<YoutubeVideo>();
 
-        Gson gson = new Gson();
+    for (JsonElement videoJsonElement : contents) {
+      JsonObject videoJsonObject = videoJsonElement.getAsJsonObject();
+      if (videoJsonObject.has("videoRenderer")) {
+        JsonObject videoData =
+            videoJsonObject.get("videoRenderer").getAsJsonObject();
 
-        JsonObject videoObject = gson.fromJson(pageContent, JsonObject.class);
+        // Initiate YoutubeVideo object instance & parse video renderer data
+        YoutubeVideo video = YoutubeVideo.getParsedVideoRenderer(videoData);
 
-        return YoutubeVideo.getParsedOEmbedObject(videoObject);
-    }
+        videos.add(video);
 
-    public YoutubePlaylist getRecommendation(String videoId) throws IOException, NoResultFoundException {
-        String requestUrl = this.YOUTUBE_BASE_URL + "watch?v=" + videoId;
-
-        String pageContent = Utils.httpRequest(requestUrl);
-
-        Pattern pattern = Pattern.compile(
-                "\"compactRadioRenderer\"(.+?)\"compactVideoRenderer\"",
-                Pattern.CASE_INSENSITIVE
-        );
-        Matcher matcher = pattern.matcher(pageContent);
-
-        if (matcher.find()) {
-            String compactRadioRenderer = matcher.group();
-            compactRadioRenderer = "{" + compactRadioRenderer.substring(0,
-                    compactRadioRenderer.length() - ",{\"compactVideoRenderer\"".length());
-
-            Gson gson = new Gson();
-
-            JsonObject compactRadioRendererObject = gson.fromJson(compactRadioRenderer, JsonObject.class)
-                                                        .get("compactRadioRenderer")
-                                                        .getAsJsonObject();
-            YoutubePlaylist youtubePlaylist = YoutubePlaylist.parseCompactRadioRenderer(compactRadioRendererObject);
-
-            return youtubePlaylist;
+        counter += 1;
+        if (counter >= maxResults) {
+          break;
         }
-        else {
-            throw new NoResultFoundException("Cannot find any recommendation for videoId: " + videoId);
-        }
+      }
     }
+    return videos;
+  }
+
+  public YoutubeVideo getInfoByVideoId(String id) throws IOException {
+    String videoUrl = this.YOUTUBE_BASE_URL + "watch?v=" + id;
+    String requestUrl = "https://noembed.com/embed?url=" + videoUrl;
+
+    String pageContent = Utils.httpRequest(requestUrl);
+
+    Gson gson = new Gson();
+
+    JsonObject videoObject = gson.fromJson(pageContent, JsonObject.class);
+
+    return YoutubeVideo.getParsedOEmbedObject(videoObject);
+  }
+
+  public YoutubeVideo getInfoByVideoUrl(String videoUrl) throws IOException {
+    String requestUrl = "https://noembed.com/embed?url=" + videoUrl;
+
+    String pageContent = Utils.httpRequest(requestUrl);
+
+    Gson gson = new Gson();
+
+    JsonObject videoObject = gson.fromJson(pageContent, JsonObject.class);
+
+    return YoutubeVideo.getParsedOEmbedObject(videoObject);
+  }
+
+  public YoutubePlaylist getRecommendation(String videoId)
+      throws IOException, NoResultFoundException {
+    String requestUrl = this.YOUTUBE_BASE_URL + "watch?v=" + videoId;
+
+    String pageContent = Utils.httpRequest(requestUrl);
+
+    Pattern pattern =
+        Pattern.compile("\"compactRadioRenderer\"(.+?)\"compactVideoRenderer\"",
+                        Pattern.CASE_INSENSITIVE);
+    Matcher matcher = pattern.matcher(pageContent);
+
+    if (matcher.find()) {
+      String compactRadioRenderer = matcher.group();
+      compactRadioRenderer =
+          "{" + compactRadioRenderer.substring(
+                    0, compactRadioRenderer.length() -
+                           ",{\"compactVideoRenderer\"".length());
+
+      Gson gson = new Gson();
+
+      JsonObject compactRadioRendererObject =
+          gson.fromJson(compactRadioRenderer, JsonObject.class)
+              .get("compactRadioRenderer")
+              .getAsJsonObject();
+      YoutubePlaylist youtubePlaylist =
+          YoutubePlaylist.parseCompactRadioRenderer(compactRadioRendererObject);
+
+      return youtubePlaylist;
+    } else {
+      throw new NoResultFoundException(
+          "Cannot find any recommendation for videoId: " + videoId);
+    }
+  }
 }
--- src/test/java/YouTubeSearchApi/TestYoutubeClientSearch.java
+++ src/test/java/YouTubeSearchApi/TestYoutubeClientSearch.java
@@ -1,28 +1,25 @@
 package YouTubeSearchApi;
 
-
 import YouTubeSearchApi.entity.YoutubeVideo;
 import YouTubeSearchApi.exception.NoResultFoundException;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TestYoutubeClientSearch {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        YoutubeClient client = new YoutubeClient();
+    YoutubeClient client = new YoutubeClient();
 
-        List<YoutubeVideo> videos;
+    List<YoutubeVideo> videos;
 
-        try {
-            videos = client.search("yoasobi", 5, "en");
-            System.out.println(videos.size());
-            System.out.println(videos.toString());
-        } catch (IOException | NoResultFoundException e) {
-            e.printStackTrace();
-        }
-
+    try {
+      videos = client.search("yoasobi", 5, "en");
+      System.out.println(videos.size());
+      System.out.println(videos.toString());
+    } catch (IOException | NoResultFoundException e) {
+      e.printStackTrace();
     }
+  }
 }
